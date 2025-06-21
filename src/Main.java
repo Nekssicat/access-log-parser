a27562fd000f7d16c30b48a4e8e1fbb65200f1f9
Ekaterina Malikova <katya15moiseeva@gmail.com>
2025-06-21 12:05:31 +0400

    Добавлены математические операции

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int x = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int y = new Scanner(System.in).nextInt();
+        System.out.println("Сумма чисел = "+(x+y));
+        System.out.println("Разность чисел (первое минус второе) = "+(x-y));
+        System.out.println("Произведение чисел = "+(x*y));
+        double del = (double) x/y;
+        System.out.println("Частное чисел (первое делить на второе) = "+del);
     }
 }
