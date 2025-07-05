9360fc75595908eb8717eda81bff0caaca97515a
Ekaterina Malikova <katya15moiseeva@gmail.com>
2025-07-05 13:03:22 +0400

    Добавлен бесконечный цикл для запроса к пути файла

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int fileNumber = 0;
+        while (true){
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            if (!fileExists) {
+                System.out.println("Такого файла не существует");
+                continue;
+            }
+
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Указана директория, а не файл");
+                continue;
+            }
+
+            fileNumber++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер "+ fileNumber);
+        }
     }
 }
